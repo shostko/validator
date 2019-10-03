545fc5a514a9836a9cf94d828bf9a79b01346cc2
Sergey Shostko <siarhei.shostka@instinctools.com>
2019-10-03 17:44:25 +0300

    add explicite return type

--- rx-validation-errors/src/main/java/by/shostko/rxvalidation/errors/Extensions.kt
+++ rx-validation-errors/src/main/java/by/shostko/rxvalidation/errors/Extensions.kt
@@ -6,10 +6,11 @@ import by.shostko.errors.Error
 import by.shostko.errors.NoError
 import by.shostko.rxvalidation.AbsValidation
 import by.shostko.rxvalidation.ValidationResult
+import io.reactivex.Flowable
 
-fun ValidationResult.error() = when (this) {
+fun ValidationResult.error(): Error = when (this) {
     ValidationResult.Valid -> NoError
     is ValidationResult.Invalid -> Error.cast(throwable ?: Throwable())
 }
 
-fun AbsValidation<*>.error() = result.map(ValidationResult::error)
+fun AbsValidation<*>.error(): Flowable<Error> = result.map(ValidationResult::error)
