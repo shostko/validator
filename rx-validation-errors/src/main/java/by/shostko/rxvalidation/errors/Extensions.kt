8bee66bf5e7357667b13585e767ca541e9a82e7f
Sergey Shostko <siarhei.shostka@instinctools.com>
2019-10-04 14:23:33 +0300

    rework errors extenstions

--- rx-validation-errors/src/main/java/by/shostko/rxvalidation/errors/Extensions.kt
+++ rx-validation-errors/src/main/java/by/shostko/rxvalidation/errors/Extensions.kt
@@ -2,15 +2,20 @@
 
 package by.shostko.rxvalidation.errors
 
+import android.content.Context
 import by.shostko.errors.Error
 import by.shostko.errors.NoError
 import by.shostko.rxvalidation.AbsValidation
 import by.shostko.rxvalidation.ValidationResult
 import io.reactivex.Flowable
 
-fun ValidationResult.error(): Error = when (this) {
+fun AbsValidation<*>.getError(): Flowable<Error> = result.map(ValidationResult::getError)
+
+fun ValidationResult.getError(): Error = when (this) {
     ValidationResult.Valid -> NoError
     is ValidationResult.Invalid -> Error.cast(throwable ?: Throwable())
 }
 
-fun AbsValidation<*>.error(): Flowable<Error> = result.map(ValidationResult::error)
+fun ValidationResult.getText(context: Context): CharSequence = getError().text(context)
+
+fun ValidationResult.getTextOrNull(context: Context): CharSequence? = getText(context).let { if (it.isBlank()) null else it }
