943cc05bf6ce80716d83ee53804b625adec4b4ed
Sergey Shostko <siarhei.shostka@instinctools.com>
2019-10-11 15:20:48 +0300

    adjust simple validator implementations

--- rx-validation-errors/src/main/java/by/shostko/rxvalidation/errors/ErrorValidator.kt
+++ rx-validation-errors/src/main/java/by/shostko/rxvalidation/errors/ErrorValidator.kt
@@ -2,6 +2,7 @@
 
 package by.shostko.rxvalidation.errors
 
+import by.shostko.errors.Error
 import by.shostko.errors.ErrorCode
 import by.shostko.rxvalidation.Validator
 
@@ -16,12 +17,51 @@ abstract class ErrorValidator<T> : Validator<T>() {
 
         protected abstract fun isValid(value: T): Boolean
     }
+
+    companion object {
+        fun <T> predicate(code: ErrorCode?, function: (T) -> Boolean): Validator<T> = SimplePredicate(code, function)
+        fun <T> predicate(function: (T) -> Boolean): Validator<T> = SimplePredicate(null, function)
+        fun <T> action(code: ErrorCode?, function: (T) -> Unit): Validator<T> = SimpleAction(code, function)
+        fun <T> action(function: (T) -> Unit): Validator<T> = SimpleAction(null, function)
+        fun <T> simple(function: (T) -> ErrorCode?): Validator<T> = SimpleCode(function)
+    }
 }
 
 private class SimplePredicate<T>(code: ErrorCode?, private val function: (T) -> Boolean) : ErrorValidator.Predicate<T>(code) {
     constructor(function: (T) -> Boolean) : this(null, function)
 
     override fun isValid(value: T): Boolean = function(value)
+}
+
+private class SimpleAction<T>(private val code: ErrorCode?, private val function: (T) -> Unit) : Validator<T>() {
+    override fun validate(value: T) {
+        try {
+            function(value)
+        } catch (th: Throwable) {
+            if (th is Error && code == null) {
+                throw th
+            } else {
+                throw Error.wrap(th, code ?: BaseValidationErrorCode(this, null))
+            }
+        }
+    }
+}
+
+private class SimpleCode<T>(private val function: (T) -> ErrorCode?) : Validator<T>() {
+    override fun validate(value: T) {
+        try {
+            val result = function(value)
+            if (result != null) {
+                throw ValidationError(result)
+            }
+        } catch (th: Throwable) {
+            if (th is Error) {
+                throw th
+            } else {
+                throw Error.wrap(th, BaseValidationErrorCode(this, null))
+            }
+        }
+    }
 }
 
 fun <T> Validator.Companion.predicate(code: ErrorCode?, function: (T) -> Boolean): Validator<T> = SimplePredicate(code, function)
--- rx-validation/src/main/java/by/shostko/rxvalidation/Validator.kt
+++ rx-validation/src/main/java/by/shostko/rxvalidation/Validator.kt
@@ -24,18 +24,43 @@ abstract class Validator<T> : Validation.Delegate<T>() {
         protected abstract fun isValid(value: T): Boolean
     }
 
-    private class SimpleAction<T>(private val function: (T) -> Unit) : Validator<T>() {
-        override fun validate(value: T) = function(value)
-    }
-
-    private class SimplePredicate<T>(message: String?, private val function: (T) -> Boolean) : Predicate<T>(message) {
-        override fun isValid(value: T): Boolean = function(value)
-    }
-
     companion object {
         fun <T> predicate(message: String?, function: (T) -> Boolean): Validator<T> = SimplePredicate(message, function)
         fun <T> predicate(function: (T) -> Boolean): Validator<T> = SimplePredicate(null, function)
-        fun <T> simple(function: (T) -> Unit): Validator<T> = SimpleAction(function)
+        fun <T> action(message: String?, function: (T) -> Unit): Validator<T> = SimpleAction(message, function)
+        fun <T> action(function: (T) -> Unit): Validator<T> = SimpleAction(null, function)
+        fun <T> simple(function: (T) -> String?): Validator<T> = SimpleMessage(function)
+    }
+}
+
+private class SimplePredicate<T>(message: String?, private val function: (T) -> Boolean) : Validator.Predicate<T>(message) {
+    override fun isValid(value: T): Boolean = function(value)
+}
+
+private class SimpleAction<T>(private val message: String? = null, private val function: (T) -> Unit) : Validator<T>() {
+    override fun validate(value: T) {
+        try {
+            function(value)
+        } catch (th: Throwable) {
+            if (message == null) {
+                throw th
+            } else {
+                throw ValidationException(message)
+            }
+        }
+    }
+}
+
+private class SimpleMessage<T>(private val function: (T) -> String?) : Validator<T>() {
+    override fun validate(value: T) {
+        try {
+            val result = function(value)
+            if (result != null) {
+                throw ValidationException(result)
+            }
+        } catch (th: Throwable) {
+            throw ValidationException()
+        }
     }
 }
 
