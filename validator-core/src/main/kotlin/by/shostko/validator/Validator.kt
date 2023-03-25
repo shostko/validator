881155ca5c239a7621f897bbf7d25031449497e1
Sergey Shostko <s.shostko@gmail.com>
2023-03-25 21:34:38 +0300

    make Composite and Mapped Validators extends from Delegated

--- validator-core/src/main/kotlin/by/shostko/validator/Validator.kt
+++ validator-core/src/main/kotlin/by/shostko/validator/Validator.kt
@@ -4,7 +4,7 @@ interface Validator<T : Any?, R : Any?> : suspend (T) -> ValidationResult<T, R>
 
 private class CompositeValidator<T : Any?, R : Any?>(
     private vararg val validators: Validator<T, R>,
-) : Validator<T, R> {
+) : DelegatedValidator<Any, T, R>() {
 
     override suspend fun invoke(value: T): ValidationResult<T, R> {
         validators.forEach { validator ->
@@ -20,7 +20,7 @@ private class CompositeValidator<T : Any?, R : Any?>(
 private class MappedValidator<T : Any?, RIN : Any?, ROUT : Any?>(
     private val validator: Validator<T, RIN>,
     private val mapper: suspend (RIN) -> ROUT,
-) : Validator<T, ROUT> {
+) : DelegatedValidator<Any, T, ROUT>() {
 
     override suspend fun invoke(value: T): ValidationResult<T, ROUT> {
         val result = validator.invoke(value)
@@ -35,8 +35,8 @@ private class MappedValidator<T : Any?, RIN : Any?, ROUT : Any?>(
 
 fun <T : Any?, R : Any?> validators(
     vararg validators: Validator<T, R>,
-): Validator<T, R> = CompositeValidator(*validators)
+): DelegatedValidator<Any, T, R> = CompositeValidator(*validators)
 
 fun <T : Any?, RIN : Any?, ROUT : Any?> Validator<T, RIN>.map(
     mapper: suspend (RIN) -> ROUT,
-): Validator<T, ROUT> = MappedValidator(this, mapper)
+): DelegatedValidator<Any, T, ROUT> = MappedValidator(this, mapper)
